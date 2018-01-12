51289a059821f1697334aa6734a9a34ac2776888
daeks <daeks@gmx.de>
2018-01-12 22:09:16 +0100

    added steam class

--- wrapper.php
+++ wrapper.php
@@ -1,4 +1,5 @@
 <?php
   define('FRAMEWORK', 'cfx');
   require_once(dirname(realpath(__FILE__)).DIRECTORY_SEPARATOR.FRAMEWORK.DIRECTORY_SEPARATOR.'common.php');
+  common::load(BASE.DIRECTORY_SEPARATOR.'include');
 ?>
