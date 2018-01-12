68a972acba060647f3aa4c474e7264bd8e6cf44f
daeks <daeks@gmx.de>
2018-01-12 19:52:45 +0100

    initial commit

---
+++ group/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  require_once(dirname(realpath(__DIR__)).DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR.'wrapper.php');
+  
+  $controller = HOME.URL_SEPARATOR.basename(__DIR__).URL_SEPARATOR.'controller.php';
+  $dialog = HOME.URL_SEPARATOR.basename(__DIR__).URL_SEPARATOR.'dialog.php';
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  require_once(dirname(realpath(__FILE__)).DIRECTORY_SEPARATOR.'common.php');
+  
+  page::start();
+  panel::dashboard();
+  page::end();
+
+?>
